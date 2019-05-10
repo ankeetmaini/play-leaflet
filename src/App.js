37a132675e0aaab9711edca8edfdf402859bad81
Ankeet Maini <ankeet.maini@gmail.com>
2019-05-11 00:46:53 +0530

    Phew, renders map :/

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from "react";
-
+import React from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    };
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng];
     return (
-      <div className="App">
-        <h2>Welcome to React</h2>
-      </div>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
 }
