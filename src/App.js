0dd4aeea99e19ef7f33117bfbf23be009e90036a
Ankeet Maini <ankeet.maini@gmail.com>
2019-05-14 19:29:18 +0530

    Fixes module systems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-
 import "leaflet/dist/leaflet.css";
 import "./App.css";
 
-import Freedraw from "./FreeDraw";
+import Freedraw from "react-leaflet-freedraw";
 
 class App extends React.Component {
   constructor() {
@@ -12,7 +11,8 @@ class App extends React.Component {
     this.state = {
       lat: 51.505,
       lng: -0.09,
-      zoom: 13
+      zoom: 13,
+      mode: window.FreeDraw.ALL
     };
   }
 
@@ -24,22 +24,55 @@ class App extends React.Component {
     console.log("handleOnMarkers: ", args);
   };
 
+  handleModeChange = (...args) => {
+    console.log("handleModeChange: ", args);
+  };
+
+  onLayerRemove = (...args) => {
+    console.log('onLayerRemove: ', args);
+  }
+
   render() {
     const position = [this.state.lat, this.state.lng];
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <Freedraw
-          mode={window.FreeDraw.ALL}
-          onMarkers={this.handleOnMarkers}
-          onModeChange={this.handleModeChange}
-        />
-      </Map>
+      <div>
+        <Map
+          center={position}
+          zoom={this.state.zoom}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+          <Freedraw
+            mode={this.state.mode}
+            onMarkers={this.handleOnMarkers}
+            onModeChange={this.handleModeChange}
+          />
+        </Map>
+        <div style={{ margin: 20 }}>
+          <button
+            onClick={() => this.setState({ mode: window.FreeDraw.CREATE })}
+          >
+            CREATE
+          </button>
+          <button onClick={() => this.setState({ mode: window.FreeDraw.EDIT })}>
+            EDIT
+          </button>
+          <button
+            onClick={() => this.setState({ mode: window.FreeDraw.DELETE })}
+          >
+            DELETE
+          </button>
+          <button
+            onClick={() => this.setState({ mode: window.FreeDraw.APPEND })}
+          >
+            APPEND
+          </button>
+        </div>
+      </div>
     );
   }
 }
